dfb49f229116908b364ecbf621f26a268a106ad3
jrb0030 <jrb0030@uah.edu>
2016-03-06 16:19:23 -0600

    Added more items

--- items.lua
+++ items.lua
@@ -15,19 +15,30 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- orange armor
+				{ x =  0, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- orange armor
 				{ x = 48, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- green armor
 				{ x = 80, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- blue armor
-				{ x = 0,  y = 15,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- black armor
+				{ x =  0, y = 15,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- black armor
 			}
 		},
 	}, -- armor
+	boot =
+	{
+		options = {
+			frames = {
+				{ x =  0, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown shoe
+				{ x = 48, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- green shoe
+				{ x = 80, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown boot
+				{ x = 96, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- gray boot
+			}
+		},
+	}, -- boot
 	chestClosed =
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square chest
-				{ x = 15.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round top chest
+				{ x =    0, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square chest
+				{ x = 15.5, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round top chest
 			}
 		},
 	}, -- chestClosed
@@ -35,8 +46,8 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square chest
-				{ x = 15.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round top chest
+				{ x =    0, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square chest
+				{ x = 15.5, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round top chest
 			}
 		},
 	}, -- chestOpen
@@ -44,14 +55,21 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x =  16,  y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown pot
-				{ x =  48,  y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver pot
-				{ x =  0,  y = 64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- empty bookcase
-				{ x =  80,  y = 64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bookcase with books #1
-				{ x =  96,  y = 64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bookcase with books #2
-				{ x =  0,  y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- chair #1
-				{ x =  16,  y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round table
-				{ x =  32,  y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- chair #2
+				{ x =    16, y =    48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown pot
+				{ x =    48, y =    48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver pot
+				{ x =     0, y =    64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- empty bookcase
+				{ x =    80, y =    64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bookcase with books #1
+				{ x =    96, y =    64,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bookcase with books #2
+				{ x =     0, y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- chair #1
+				{ x =    16, y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round table
+				{ x =    32, y = 111.5,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- chair #2
+				{ x =     0, y =   192,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- white skull
+				{ x =    16, y =   192,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- pile of white bones
+				{ x =     0, y =   207,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- white animal skull
+				{ x =    16, y =   207,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- white rib cage
+				{ x =  -0.5, y =   271,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- RIP tombstone
+				{ x =  31.5, y =   271,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- skull tombstone
+				{ x =  47.5, y =   271,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- blank tombstone
 			}
 		},
 	}, -- decor
@@ -59,25 +77,45 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x =  0,  y =   0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --wood door	
-				{ x = 16,  y =   0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --wood locked door
-				{ x =  0,  y =  16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --metal door	
-				{ x = 16,  y =  16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --metal locked door	
-				{ x =  0,  y =  80,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --open Path
+				{ x =  0, y =   0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --wood door	
+				{ x = 16, y =   0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --wood locked door
+				{ x =  0, y =  16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --metal door	
+				{ x = 16, y =  16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --metal locked door	
+				{ x =  0, y =  80,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, --open path
 			}
 		},
 	}, -- door
+	hat =
+	{
+		options = {
+			frames = {
+				{ x = 32, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver helmet w/ red feather
+				{ x = 64, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver helmet w/ gold buzz
+				{ x = 80, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- helmet closed faceplate
+				{ x = 32, y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- skyrim helmet
+			}
+		},
+	}, -- hat
+	key =
+	{
+		options = {
+			frames = {
+				{ x =  0, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver key	
+				{ x = 15, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- gray key
+			}
+		},
+	}, -- key
 	potion =
 	{
 		options = {
 			frames = {
-				{ x =  0,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- red 
-				{ x =  15.5,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- pink 
-				{ x =  31,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- orange 
-				{ x =  47,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- yellow 
-				{ x =  80,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- dark green 
-				{ x =  0,  y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- purple 
-				{ x =  0,  y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- gray 
+				{ x =    0, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- red 
+				{ x = 15.5, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- pink 
+				{ x =   31, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- orange 
+				{ x =   47, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- yellow 
+				{ x =   80, y =  0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- dark green 
+				{ x =    0, y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- purple 
+				{ x =    0, y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- gray 
 			}
 		},
 	}, -- potion
@@ -85,12 +123,12 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bronze
-				{ x = 15.5,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- green with yellow outline
-				{ x = 31.5,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown with yellow symbol
-				{ x = 47.5,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver with small image
-				{ x = 80,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round bronze
-				{ x = 95.5,  y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- blue
+				{ x =    0, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- bronze
+				{ x = 15.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- green with yellow outline
+				{ x = 31.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- brown with yellow symbol
+				{ x = 47.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- silver with small image
+				{ x =   80, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round bronze
+				{ x = 95.5, y = 0,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- blue
 			}
 		},
 	}, -- shield
@@ -98,10 +136,10 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- spear trap
-				{ x = 47,  y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- spikes
-				{ x = 64,  y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round hole
-				{ x = 80,  y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square hole
+				{ x =  0, y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- spear trap
+				{ x = 47, y = 48,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- spikes
+				{ x = 64, y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- round pit
+				{ x = 80, y = 32,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- square pit
 			}
 		},
 	}, -- trap
@@ -109,11 +147,11 @@ local ItemOptionsTable = {
 	{
 		options = {
 			frames = {
-				{ x = 0,  y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- sword #1
+				{ x =  0, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- sword #1
 				{ x = 16, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- sword #2
 				{ x = 48, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- sword #3
 				{ x = 64, y = -1,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- sword #4
-				{ x = 0, y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- single-sided axe
+				{ x =  0, y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- single-sided axe
 				{ x = 16, y = 16,  width = IMAGE_WIDTH, height = IMAGE_HEIGHT}, -- single-sided axe
 			}
 		},
@@ -127,6 +165,11 @@ ItemsTable = {
 		sheet = graphics.newImageSheet( "images/Items/Armor.png", 
 			ItemOptionsTable.armor.options ),
 	}, -- armor
+	boot =
+	{
+		sheet = graphics.newImageSheet( "images/Items/Boot.png", 
+			ItemOptionsTable.boot.options ),
+	}, -- boot
 	chestClosed =
 	{
 		sheet = graphics.newImageSheet( "images/Items/Chest0.png", 
@@ -147,6 +190,16 @@ ItemsTable = {
 		sheet = graphics.newImageSheet( "images/Objects/Door0.png", 
 			ItemOptionsTable.door.options ),
 	}, -- door
+	hat =
+	{
+		sheet = graphics.newImageSheet( "images/Items/Hat.png", 
+			ItemOptionsTable.hat.options ),
+	}, -- hat
+	key =
+	{
+		sheet = graphics.newImageSheet( "images/Items/Key.png", 
+			ItemOptionsTable.key.options ),
+	}, -- key
 	potion =
 	{
 		sheet = graphics.newImageSheet( "images/Items/Potion.png", 
