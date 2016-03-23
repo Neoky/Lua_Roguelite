7565829e6b7fd63efe14184178ced0e7b4100493
unknown <mwcox@neuronglobal.com>
2016-03-23 17:38:34 -0500

    made decent player save and load functions

--- scripts/player.lua
+++ scripts/player.lua
@@ -9,6 +9,8 @@ xPos and yPos - hopefully be used store map postion.
 map={} - The map should be the parent of the player, so I want to store it to reference it.
 ]]--------------------------------------
 
+local loadtable = require('scripts.saveGame')
+
 Player = {tag="player", hpCur=100, hpMax=100, attack=2, keys=0, rKey=0, gKey=0, bKey=0, xPos=1, yPos=1, map={}, tileSheet={}, arrows={}}
 
 local spriteOpt =
@@ -28,6 +30,7 @@ local spriteSeqData = {
 function Player:new(o)
 	setmetatable(o, self);
 	self.__index = self;
+	--o:save()
 	return o;
 end
 
@@ -39,6 +42,8 @@ function Player:spawn()
 	self.body.x = xPos;
 	self.body.y = yPos;
 	self.hpCur = hpCur;
+
+	self:load()
 
 	return self.body
 end
@@ -73,14 +78,46 @@ function Player:move(xPos, yPos)
 		self.xPos, self.yPos = xPos, yPos
 		self.body.x = self.map.mapArray[xPos][yPos].x
 		self.body.y = self.map.mapArray[xPos][yPos].y
-		-- Create shortcuts
-		xVal, yVal = self.xPos, self.yPos;
-		mapArray = self.map.mapArray
 
 		self.map:enemyTurn()
 	end
 
 	return previousX, previousY
+end
+
+function Player:save()
+	-- make new table with player stats I want to save.
+	-- call save game function with new table.
+	local t = {
+		["attack"] = self.attack,
+		["hpCur"] = self.hpCur,
+		["hpMax"] = self.hpMax,
+		["keys"] = self.keys,
+		["rKey"] = self.rKey,
+		["gKey"] = self.gKey,
+		["bKey"] = self.bKey,
+		["xPos"] = self.xPos,
+		["yPos"] = self.yPos
+	}
+	loadtable.saveTable(t, "rogue_save.json")
+end
+
+function Player:load()
+	-- call load game function and save output to new var table.
+	-- update current player object with values loaded in loadGame variable.
+	t = loadtable.loadTable("rogue_save.json")
+	if t == nil then
+		return nil
+	end
+	self.attack = t.attack
+	self.hpCur = t.hpCur
+	self.hpMax = t.hpMax
+	self.keys = t.keys
+	self.rKey = t.rKey
+	self.gKey = t.gKey
+	self.bKey = t.bKey
+	self.xPos = t.xPos
+	self.yPos = t.yPos
 end
 
 return Player;
