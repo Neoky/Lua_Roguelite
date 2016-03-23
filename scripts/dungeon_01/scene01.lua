2dfbdab875bf05e6470a573dd0c4ab66a87d78f4
Steven Muller <Steven Muller>
2016-03-23 14:42:48 -0500

    Modified the way rooms are made

--- scripts/dungeon_01/scene01.lua
+++ scripts/dungeon_01/scene01.lua
@@ -2,7 +2,7 @@
 local composer = require("composer")
 local scene = composer.newScene()
 local Map = require("scripts.map")
-local File = require("scripts.saveGame")
+--local File = require("scripts.saveGame")
 --local Items, Player, Enemy, enemyClass, Map = require('scripts.standardAssets')
 local map1
 
@@ -16,39 +16,39 @@ function scene:create()
 	--local loadedSettings = loadsave.loadTable( "settings.json" )
 
 	--Make a default room with doors
-	mapArray = map1:makeRoom(true, true, true, true)
+	--mapArray = map1:makeRoom(true, true, true, true)
 	--map1 = file
-	creatorList = fil
+	--creatorList = fil
 	--print(mapArray[1][1].x)
 	--print(mapArray[1][1].passable)
 
 	--Swap out a tile with a different one
 	--
-	mapArray[10][4] = map1:swapTile("door", 1, 10, 4, false)
+	--mapArray[10][4] = map1:swapTile("door", 1, 10, 4, false)
 
-	print(mapArray[10][4].x)
-	print(mapArray[10][4].passable)
+	--print(mapArray[10][4].x)
+	--print(mapArray[10][4].passable)
 
 	--Place an object on top of a tile. Can specify if passable or pushable
-	pot = map1:placeObject("object","decor", 2, 3, 2, false, true)
+	--pot = map1:placeObject("object","decor", 2, 3, 2, false, true)
 
 	--Place the player character and generate arrows around him if possible
 
-	print("Pot is at " .. pot.x .. "," .. pot.y)
-	print(pot.passable)
-	print(pot.pushable)
+	--print("Pot is at " .. pot.x .. "," .. pot.y)
+	--print(pot.passable)
+	--print(pot.pushable)
 	--]]--
 	--List to swap out tile
 	--
 	creatorList = 
 	{
 		--type, frameNum, x, y, passable
-		[1] = {"tile", 2, 3, 4, true}, --TODO: This tiles specifically causes the player to be unable to move above it. Investigate
+		[1] = {"tile", 2, 3, 4, true},
 		[2] = {"tile", 2, 5, 5, true},
 	}
 
 	--Swap out any of the default tiles with custom ones
-	mapArray = map1:buildMap(creatorList)
+	--mapArray = map1:buildMap(creatorList)
 
 	--List to add objects layered on top of map
 	objectList = 
@@ -59,11 +59,18 @@ function scene:create()
 		[3] = {"item","armor", 4, 11, 3, true, false}, 
 		[4] = {"item","potion", 7, 3, 7, true, false},
 		[5] = {"enemy","undead", 1, 8, 6, false, false},
+		[6] = {"door", "door", 1, 1, 5, true, false},
+		[7] = {"door", "door", 1, 13, 5, true, false},
+		[8] = {"door", "door", 1, 7, 1, true, false},
+		[9] = {"door", "door", 1, 7, 9, true, false},
 		--[6] = {"enemy","demon", 1, 1, 4, false, false},
 	}
 
 	--Fill the map will objects on the tiles
-	map1:fillMap(objectList)
+	--map1:fillMap(objectList)
+
+	map1:generateMap(1, "grayWall", creatorList, objectList)
+
 	player = map1:placePlayer("player", 1, 6, 6)
 	--]]--
 end
