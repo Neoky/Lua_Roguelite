063833d8ae95e787bded5e5ca5e182a5a62e0459
jrb0030 <jrb0030@uah.edu>
2016-04-10 15:17:13 -0500

    Added more decor

--- scripts/dungeon_01/scene14.lua
+++ scripts/dungeon_01/scene14.lua
@@ -32,11 +32,17 @@ function scene:create()
 		-- Extra Item Args: power
 		[1] = {"door",  "door",      1, 7, 9, true, false,  "scene13", 7, 2}, -- bottom
 		[2] = {"enemy", "redDemon",  2, 7, 4, true, false, 100, 25, "STAND"}, -- boss
-		[3] = {"item", "decor",     16, 3, 3, false, false},  -- white skull
-		[4] = {"item", "decor",     17, 3, 6, false, false},  -- pile of white bones
-		[5] = {"item", "decor",     18, 9, 2, false, false},  -- white animal skull
-		[6] = {"item", "decor",     19, 10, 3, false, false},  -- white rib cage
-		[7] = {"item", "decor",     16, 9, 6, false, false},  -- white skull
+		[3] = {"item",  "decor",    40, 3, 1, false, false}, -- wall candles
+		[4] = {"item",  "decor",    40, 4, 1, false, false}, -- wall candles
+		[5] = {"item",  "decor",    40, 5, 1, false, false}, -- wall candles
+		[6] = {"item",  "decor",    40, 9, 1, false, false}, -- wall candles
+		[7] = {"item", "decor",     40, 10, 1, false, false}, -- wall candles
+		[8] = {"item", "decor",     40, 11, 1, false, false}, -- wall candles
+		[9] = {"item", "decor",     16, 3, 3, false, false},  -- white skull
+		[10] = {"item", "decor",    17, 3, 6, false, false},  -- pile of white bones
+		[11] = {"item", "decor",    18, 9, 2, false, false},  -- white animal skull
+		[12] = {"item", "decor",    19, 10, 3, false, false},  -- white rib cage
+		[13] = {"item", "decor",    16, 9, 6, false, false},  -- white skull
 	}
 end
 
